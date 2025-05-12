9bc100325bcf950084db1bf7275ea9263991a98d
dotPaulo <psikys2308@gmail.com>
2025-05-12 14:45:55 +0100

    PHPMailer

--- admin/send_email.php
+++ admin/send_email.php
@@ -7,9 +7,32 @@ $headers = "From: paul0.oliveir42308@gmail.com\r\n";
 $headers .= "Reply-To: paul0.oliveir42308@gmail.com\r\n";
 $headers .= "Content-Type: text/plain; charset=UTF-8";
 
-if (mail($para, $assunto, $mensagem, $headers)) {
-    echo "<div style='padding:20px; font-family:sans-serif;'>E-mail enviado com sucesso. <a href='mensagens.php'>Voltar</a></div>";
-} else {
-    echo "<div style='padding:20px; font-family:sans-serif; color:red;'>Erro ao enviar e-mail. <a href='mensagens.php'>Voltar</a></div>";
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require '../vendor/autoload.php';
+
+$mail = new PHPMailer(true);
+
+try {
+    $mail->isSMTP();
+    $mail->Host = 'smtp.gmail.com';
+    $mail->SMTPAuth = true;
+    $mail->Username = 'paul0.oliveir42308@gmail.com';  // Seu e-mail
+    $mail->Password = 'nnbb janf kkba flmf';           // App password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+    $mail->Port = 587;
+
+    $mail->setFrom('paul0.oliveir42308@gmail.com', 'Administrador');
+    $mail->addAddress($_POST['to']);
+
+    $mail->Subject = $_POST['subject'];
+    $mail->Body    = $_POST['body'];
+
+    $mail->send();
+    echo "<div style='color:green;'>E-mail enviado com sucesso. <a href='mensagens.php'>Voltar</a></div>";
+} catch (Exception $e) {
+    echo "<div style='color:red;'>Erro ao enviar e-mail: {$mail->ErrorInfo}. <a href='mensagens.php'>Voltar</a></div>";
 }
+?>
 ?>
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'd29f4846e56720519861acbd2fb0ad8b49936143',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'd29f4846e56720519861acbd2fb0ad8b49936143',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
@@ -25,6 +25,15 @@
             'reference' => '500501c2ce893c824c801da135d02661199f60c5',
             'type' => 'library',
             'install_path' => __DIR__ . '/../firebase/php-jwt',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'v6.10.0',
+            'version' => '6.10.0.0',
+            'reference' => 'bf74d75a1fde6beaa34a0ddae2ec5fce0f72a144',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
             'aliases' => array(),
             'dev_requirement' => false,
         ),
