f7adf26416a8f18f8f7358ab825852f44c94ee9b
dotPaulo <psikys2308@gmail.com>
2025-05-13 12:54:11 +0100

    Ideia de logs na dashbord

--- admin/include/header.php
+++ admin/include/header.php
@@ -105,6 +105,11 @@ if ($inbox) {
                             </div>
                             Dashboard
                         </a>
+                        <a class="nav-link" href="#.php">
+                            <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt " style="color: #fff"></i>
+                            </div>
+                            Relatórios
+                        </a>
                         <div class="sb-sidenav-menu-heading">GESTÃO</div>
                         <a class="nav-link collapsed" href="#" data-bs-toggle="collapse"
                             data-bs-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
